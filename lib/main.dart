be93ec2704311508ca69a58979690a479ddf0377
uchida-masanori <76242252+uchida-masanori@users.noreply.github.com>
2022-07-05 22:30:05 +0900

    API通信周りの仮実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:holotube_app/view/home_page.dart';
+import 'package:holotube_app/view/view.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HoloTube',
-      theme: ThemeData(primaryColorLight: Colors.lightBlue),
-      home: const HomePage(),
-    );
-  }
-}
+void main() => runApp(const MyApp());
